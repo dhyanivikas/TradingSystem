ec8567b5701e7978eb6a308598fc6813d1ab6cd6
Vikas Dhyani <vikasdhyani@Viking-MBP.attlocal.net>
2024-06-14 21:29:49 -0700

    adding data extraction logic

--- data_extraction/data_extractors.py
+++ data_extraction/data_extractors.py
@@ -25,3 +25,50 @@ def get_realtime_stock_price(stock_symbol, rapid_api_key):
     price_data = response.json()
     price = float(price_data['price'])
     return price
+
+
+def fetch_adx_data(stock_symbol, interval, output_size, rapid_api_key):
+    base_url = TWELVE_API_BASE_URL
+    rapid_api_host = RAPID_API_HOST
+    url = f"{base_url}/adx"
+    querystring = {"symbol":{stock_symbol},"interval":{interval},"format":"json","outputsize":{output_size},"time_period":"14"}
+    headers = {
+        "x-rapidapi-key": rapid_api_key,
+        "x-rapidapi-host": rapid_api_host
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
+
+
+
+def fetch_ohlcv_data(stock_symbol, interval, output_size, rapid_api_key):
+    base_url = TWELVE_API_BASE_URL
+    rapid_api_host = RAPID_API_HOST
+    url = f"{base_url}/time_series"
+
+    querystring = {"symbol":{stock_symbol},"interval":{interval},"format":"json","outputsize":{output_size}}
+    headers = {
+        "x-rapidapi-key": rapid_api_key,
+        "x-rapidapi-host": rapid_api_host
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
+
+
+def fetch_bbw_data(stock_symbol, interval, standard_deviation, time_period, output_size, rapid_api_key):
+    base_url = TWELVE_API_BASE_URL
+    rapid_api_host = RAPID_API_HOST
+    url = f"{base_url}/bbands"
+    print("parameters for data extraction:-> ", stock_symbol, interval, standard_deviation, time_period, output_size)
+
+    querystring = {"symbol": {stock_symbol}, "interval": {interval}, "sd": {standard_deviation}, "series_type": "close",
+                   "ma_type": "SMA", "time_period": {time_period}, "outputsize": {output_size}, "format": "json"}
+
+    headers = {
+        "x-rapidapi-key": rapid_api_key,
+        "x-rapidapi-host": rapid_api_host
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    return response.json()
